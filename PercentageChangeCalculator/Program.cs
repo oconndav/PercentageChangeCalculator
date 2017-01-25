fddf1592f7eb1e82e2ab84d5a36e1ac210776712
david <oconndav@hotmail.com>
2017-01-25 21:52:52 +1100

    Added basic user interface

--- PercentageChangeCalculator/Program.cs
+++ PercentageChangeCalculator/Program.cs
@@ -10,7 +10,18 @@ namespace PercentageChangeCalculator
     {
         static void Main()
         {
-            Console.WriteLine("Just testing");
+            Console.WriteLine("Welcome to our simple Percentage Change Calculator App");
+
+            Console.WriteLine("Please enter the original number: ");
+            decimal numberOrig = readInputNumber();
+
+            Console.WriteLine("Please enter the new number: ");
+            decimal numberNew = readInputNumber();
+
+            decimal result = CalculatePercentageChange(numberOrig, numberNew);
+
+            Console.WriteLine($"The result is: {result}");
+            Console.ReadKey();   
         }
 
         public static decimal CalculatePercentageChange(decimal numberOrig, decimal numberNew)
@@ -20,6 +31,13 @@ namespace PercentageChangeCalculator
             percentageChangeResult = ((numberNew - numberOrig) / numberOrig) * 100;
 
             return percentageChangeResult;
+        }
+
+        public static decimal readInputNumber()
+        {
+            decimal readInputnumber = decimal.Parse(Console.ReadLine());
+
+            return readInputnumber;
         }
 
     }
